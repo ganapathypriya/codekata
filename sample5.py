7261c505152651e4d07d541c9decee2ed3e9ac41
ganapathypriya <47441033+ganapathypriya@users.noreply.github.com>
2019-02-08 15:00:22 +0530

    Create sample5.py

---
+++ sample5.py
@@ -0,0 +1,9 @@
+Inputvalue=raw_input()
+a,b,c=Inputvalue.split(" ")
+a,b,c=int(a),int(b),int(c)
+if((a>b) and(a>c)):
+    print(a)
+elif((b>c)and(b>a)):
+    print(b)
+elif((c>a)and(c>b)):
+     print(c)
